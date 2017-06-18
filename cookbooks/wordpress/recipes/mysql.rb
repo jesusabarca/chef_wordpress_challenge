e89ed3cb19d310496cb725878c90e11d749512fc
J Jesús Abarca Vargas <jesus@abarca.xyz>
2017-06-18 15:52:28 -0500

    Installs and configures Mysql

--- cookbooks/wordpress/recipes/mysql.rb
+++ cookbooks/wordpress/recipes/mysql.rb
@@ -3,3 +3,34 @@
 # Recipe:: mysql
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+MYSQL_CREDS = {
+  root: {
+    pwd: 'incredibly_secure_password'
+  },
+  wordpress: {
+    db: 'wordpress',
+    user: 'wordpressuser',
+    pwd: 'another_ultrasecure_password'
+  }
+}.freeze
+
+bash 'set_insecure_mysql_password' do
+  code <<-BASH
+    echo 'mysql-server mysql-server/root_password password #{MYSQL_CREDS[:root][:pwd]}' | sudo debconf-set-selections
+    echo 'mysql-server mysql-server/root_password_again select #{MYSQL_CREDS[:root][:pwd]}' | debconf-set-selections
+  BASH
+end
+
+package 'mysql-server'
+
+service 'mysql' do
+  action [:start, :enable]
+end
+
+bash 'setup_wordpress_db' do
+  code <<-BASH
+    mysql -u root --password='#{MYSQL_CREDS[:root][:pwd]}' -e "CREATE DATABASE #{MYSQL_CREDS[:wordpress][:db]} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+    mysql -u root --password='#{MYSQL_CREDS[:root][:pwd]}' -e "GRANT ALL ON #{MYSQL_CREDS[:wordpress][:db]}.* TO '#{MYSQL_CREDS[:wordpress][:user]}'@'localhost' IDENTIFIED BY '#{MYSQL_CREDS[:wordpress][:pwd]}';"
+    mysql -u root --password='#{MYSQL_CREDS[:root][:pwd]}' -e "FLUSH PRIVILEGES;"
+  BASH
+end
--- cookbooks/wordpress/test/smoke/default/mysql_test.rb
+++ cookbooks/wordpress/test/smoke/default/mysql_test.rb
@@ -4,15 +4,28 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+MYSQL_CREDS = {
+  root: {
+    pwd: 'incredibly_secure_password'
+  },
+  wordpress: {
+    db: 'wordpress',
+    user: 'wordpressuser',
+    pwd: 'another_ultrasecure_password'
+  }
+}.freeze
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package('mysql-server') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service('mysql') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+sql = mysql_session("#{MYSQL_CREDS[:wordpress][:user]}", "#{MYSQL_CREDS[:wordpress][:pwd]}")
+describe sql.query("show databases like \'wordpress\';") do
+  its('stdout') { should match(/wordpress/) }
 end
